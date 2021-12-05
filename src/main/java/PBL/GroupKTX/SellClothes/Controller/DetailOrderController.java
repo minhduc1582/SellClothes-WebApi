f102c94e413bdbdd381c9736c74a42a71eb6b86e
minhduc1582 <62060849+minhduc1582@users.noreply.github.com>
2021-12-05 23:43:14 +0700

    final

--- src/main/java/PBL/GroupKTX/SellClothes/Controller/DetailOrderController.java
+++ src/main/java/PBL/GroupKTX/SellClothes/Controller/DetailOrderController.java
@@ -3,11 +3,15 @@ package PBL.GroupKTX.SellClothes.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import PBL.GroupKTX.SellClothes.Model.DetailOrder;
 import PBL.GroupKTX.SellClothes.Model.Repository.DetailOrderRepository;
 
 @RestController
@@ -17,12 +21,32 @@ public class DetailOrderController {
 	DetailOrderRepository detailOrderRepository;
 	
 	@GetMapping("")
-	public ResponseEntity<?> getAllDetailsOrders(){
+	public ResponseEntity<?> getAllDetailOrders(){
 		return ResponseEntity.status(HttpStatus.OK).body(detailOrderRepository.findAll());
 	}
 	@GetMapping("/getbypidandoid")
 	public ResponseEntity<?> getAllDetailsByPidAndOid(String idOrder,String productID){
 		return ResponseEntity.status(HttpStatus.OK).body(detailOrderRepository.getDetailOrdersByPidandOid(idOrder, productID));
+	}
+	@PostMapping("/add")
+	public ResponseEntity<?> addDetailOrders(@RequestBody DetailOrder detailOrder){
+		try {
+			return ResponseEntity.status(HttpStatus.OK).body(detailOrderRepository.save(detailOrder));
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.status(HttpStatus.OK).body(new DetailOrder());
+		}
+	}
+	@DeleteMapping("/delete")
+	public ResponseEntity<?> deleteDetailOrders(@RequestParam int id){
+		try {
+			detailOrderRepository.deleteById(id);
+			return ResponseEntity.status(HttpStatus.OK).body("Delete Success");
+		} catch (Exception e) {
+			// TODO: handle exception
+			return ResponseEntity.status(HttpStatus.OK).body("Delete Fail");
+		}
 	}
 //	@GetMapping("/getbyphone")
 //	public ResponseEntity<?> getDetailOrdersByPhoneUser(@RequestParam String Phone){
