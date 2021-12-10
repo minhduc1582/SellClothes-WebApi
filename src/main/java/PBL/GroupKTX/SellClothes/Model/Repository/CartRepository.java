c86b160fe30e2319dc61ee483ceea968107ab104
minhduc1582 <62060849+minhduc1582@users.noreply.github.com>
2021-12-10 15:15:09 +0700

    last

--- src/main/java/PBL/GroupKTX/SellClothes/Model/Repository/CartRepository.java
+++ src/main/java/PBL/GroupKTX/SellClothes/Model/Repository/CartRepository.java
@@ -3,13 +3,16 @@ package PBL.GroupKTX.SellClothes.Model.Repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import PBL.GroupKTX.SellClothes.Model.Dto.Cart;
+import PBL.GroupKTX.SellClothes.Model.Orders;
+
+
 
 @Repository
-public interface CartRepository extends JpaRepository<Cart, Integer>{
+public interface CartRepository extends JpaRepository<Orders, Integer>{
 	@Query(value="SELECT product.pid, product.pname, product.price, detailorder.quantity, detailorder.idDetailorder FROM orders "
 			+ "INNER JOIN detailorder ON orders.idOrder = detailorder.idOrder "
 			+ "INNER JOIN product on detailorder.pid = product.pid WHERE orders.uID = ?1",nativeQuery = true)
